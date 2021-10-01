b95d4dc62254ed7560ee210ca91bf82fe1651041
ronentgreat <ronenh53@gmail.com>
2021-10-01 13:34:36 +0000

    start

--- src/com/RonenTGreat/Main.java
+++ src/com/RonenTGreat/Main.java
@@ -23,10 +23,54 @@ public class Main {
                 System.out.println("Good! Let's start. You chose Star Wars!");
                 System.out.println("Please enter the following inputs.");
                 System.out.print("Enter an adjective: ");
-                String adjective1 = keyboard.nextLine();
+                String adjective1 = keyboard.next();
                 System.out.print("Enter a noun: ");
-
-
+                String noun1 = keyboard.next();
+                System.out.print("Enter an adjective: ");
+                String adjective2 = keyboard.next();
+                System.out.print("Enter a noun(place): ");
+                String noun2 = keyboard.next();
+                System.out.print("Enter an adjective: ");
+                String adjective3 = keyboard.next();
+                System.out.print("Enter an adjective: ");
+                String adjective4 = keyboard.next();
+                System.out.print("Enter a plural noun(vehicle): ");
+                String noun3 = keyboard.next();
+                System.out.print("Enter an adjective: ");
+                String adjective5 = keyboard.next();
+                System.out.print("Enter an adjective: ");
+                String adjective6 = keyboard.next();
+                System.out.print("Enter a plural noun: ");
+                String noun4 = keyboard.next();
+                System.out.print("Enter an adjective: ");
+                String adjective7 = keyboard.next();
+                System.out.print("Enter a plural noun: ");
+                String noun5 = keyboard.next();
+                System.out.print("Enter a plural noun: ");
+                String noun6 = keyboard.next();
+                System.out.print("Enter an adjective: ");
+                String adjective8 = keyboard.next();
+                System.out.print("Enter a noun: ");
+                String noun7 = keyboard.next();
+                System.out.print("Enter a verb: ");
+                String verb1 = keyboard.next();
+                System.out.print("Enter an adjective: ");
+                String adjective9 = keyboard.next();
+                System.out.print("Enter a verb: ");
+                String verb2 = keyboard.next();
+                System.out.print("Enter a plural noun: ");
+                String noun8 = keyboard.next();
+                System.out.print("Enter a plural noun(type of job): ");
+                String noun9 = keyboard.next();
+                System.out.print("Enter an adjective: ");
+                String adjective10 = keyboard.next();
+                System.out.print("Enter a verb: ");
+                String verb3 = keyboard.next();
+                System.out.print("Enter an adjective: ");
+                String adjective11 = keyboard.next();
+                System.out.println("----------------");
+                System.out.println("Star Wars Mad Libs");
+                System.out.println("Star");
 
             }
 
