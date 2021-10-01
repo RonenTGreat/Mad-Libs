9a1de5a293f021e99861d701131932a0a5c033c5
ronentgreat <ronenh53@gmail.com>
2021-10-01 14:31:31 +0000

    added an ASCII art to it

--- src/com/RonenTGreat/Main.java
+++ src/com/RonenTGreat/Main.java
@@ -70,28 +70,32 @@ public class Main {
                 String adjective11 = keyboard.next();
 
                 System.out.println("-------------------------------------------------");
-                System.out.println("      ________________.  ___     .______\n" +
-                        "     /                | /   \\    |   _  \\\n" +
-                        "    |   (-----|  |----`/  ^  \\   |  |_)  |\n" +
-                        "     \\   \\    |  |    /  /_\\  \\  |      /\n" +
-                        ".-----)   |   |  |   /  _____  \\ |  |\\  \\-------.\n" +
-                        "|________/    |__|  /__/     \\__\\| _| `.________|\n" +
-                        " ____    __    ____  ___     .______    ________.\n" +
-                        " \\   \\  /  \\  /   / /   \\    |   _  \\  /        |\n" +
-                        "  \\   \\/    \\/   / /  ^  \\   |  |_)  ||   (-----`\n" +
-                        "   \\            / /  /_\\  \\  |      /  \\   \\\n" +
-                        "    \\    /\\    / /  _____  \\ |  |\\  \\---)   |\n" +
-                        "     \\__/  \\__/ /__/     \\__\\|__| `._______/\n" +
-                        "\n" +
-                        "------------------------------------------------\n");
-                System.out.printf("Star War is a %s %s of %s versus evil in a %s far far away.%n", adjective1, noun1, adjective2, noun2);
-                System.out.println("There are " + adjective3 + " battles between " + adjective4 + " " + noun3 + " in " + adjective5 + " space and " + adjective6 + " duels with " + noun4 + " called " + adjective7 + " sabers.");
+                System.out.println("""
+                              ________________.  ___     .______
+                             /                | /   \\    |   _  \\
+                            |   (-----|  |----`/  ^  \\   |  |_)  |
+                             \\   \\    |  |    /  /_\\  \\  |      /
+                        .-----)   |   |  |   /  _____  \\ |  |\\  \\-------.
+                        |________/    |__|  /__/     \\__\\| _| `.________|
+                         ____    __    ____  ___     .______    ________.
+                         \\   \\  /  \\  /   / /   \\    |   _  \\  /        |
+                          \\   \\/    \\/   / /  ^  \\   |  |_)  ||   (-----`
+                           \\            / /  /_\\  \\  |      /  \\   \\
+                            \\    /\\    / /  _____  \\ |  |\\  \\---)   |
+                             \\__/  \\__/ /__/     \\__\\|__| `._______/
+
+                        ------------------------------------------------
+                        """);
+                System.out.printf("Star War is a %s %s of %s versus evil in a %s far far away.%n", adjective1, noun1,
+                        adjective2, noun2);
+                System.out.println("There are " + adjective3 + " battles between " + adjective4 + " " + noun3 + " in "
+                        + adjective5 + " space and " + adjective6 + " duels with " + noun4 + " called " + adjective7 + " sabers.");
                 System.out.printf("%s called \"droids\" are helpers and %s" +
                         " to the heroes.%n", noun5.toUpperCase(), noun6);
-                System.out.printf("A %s power called the %s %ss people to do %s " +
-                        " things, like %s %s.%n",adjective8, noun6.toUpperCase(),verb1,adjective9,verb2,noun7);
+                System.out.printf("A %s power called The %s %ss people to do %s " +
+                        " things, like %s %s.",adjective8, noun7.toUpperCase(),verb1,adjective9,verb2,noun8);
                 System.out.printf("The Jedi %s use The Force for %s side and the Sith %s it for the %s side.%n",
-                        noun8,adjective10, verb3, adjective11);
+                        noun9,adjective10, verb3, adjective11);
 
             }
 
